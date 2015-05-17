2d15c50216caa712ca44d98785abd0d4b6234e2e
Diana Polonskaya <polonskaya.d@mail.ru>
2015-05-17 18:23:02 +0300

    version 17/05/2015

---
+++ content.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+		    $("#content").fancybox({
+		        'titlePosition' : 'inside',
+		        'transitionIn' : 'none',
+		        'transitionOut' : 'none',
+		        'centerOnScroll': 'true',
+		        'hideOnOverlayClick': 'false',
+		        'autoScale': 'true'
+		    });
+		});
